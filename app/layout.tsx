4a14a0ad8af51c68aae99e404e6e8a1ee193b722
GabrielbrandaoRP <gabrielhand15@gmail.com>
2026-04-08 11:02:49 -0300

    feat:Add menu bar hamburguer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { jetbrainsMono } from './fonts';
 import NavBar from "./_components/nav-bar";
 import { AosInit } from "./_components/aos-init";
-import { Raleway, JetBrains_Mono } from 'next/font/google'
+import { Raleway } from 'next/font/google'
 import { cn } from "@/lib/utils";
 
-const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'});
 
 export const metadata: Metadata = {
   title: "Gabriel Brandão - Desenvolvedor Front-end",
   description: "Front-end Developement",
 };
 
-// export const tektur = Tektur({ 
-//   subsets: ['latin'],
-//   display: 'swap',
-// });
 export const raleway = Raleway({ 
   subsets: ['latin'],
   display: 'swap',
@@ -28,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn(raleway.className, "font-mono", jetbrainsMono.variable)}>
+    <html lang="en" className={cn(raleway.className, "font-mono")}>
       <body>
         <NavBar/>
         {children}
