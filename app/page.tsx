15d16f3f6456840ac063e87e6b3eda7da1202ae7
GabrielbrandaoRP <gabrielhand15@gmail.com>
2026-04-08 18:15:56 -0300

    fix:remove scroll horizontal

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import Works from "./_components/works";
 
 export default function Home() {
   return (
-    <main className="flex flex-col w-full">
+    <main className="flex flex-col ">
       <div className="flex flex-col bg-linear-to-b from-cyan-50 from-25% via-zinc-300 via-75% to-zinc-500 to-100% 
       dark:bg-linear-to-b dark:from-gray-900 dark:from-25% dark:via-zinc-500 dark:via-75% dark:to-zinc-700 dark:to-100%
       pb-18">
